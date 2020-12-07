c359670b08c8dcaa2a63d8cb979ed9bd9f6a0323
Waku <watsonmi188@gmail.com>
2020-12-07 02:57:47 +0000

    fixed

--- run.cpp
+++ run.cpp
@@ -15,7 +15,7 @@ bool replace(std::string& str, const std::string& from, const std::string& to) {
 int main (int argc, char** argv) {
 	int status;
 	cout << argc << endl;
-  if (argc != 1) {
+  if (argc != 2) {
 		cout << "Usage: hexa <file>\n";
 		status = 1;
 	} else {
