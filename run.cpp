98f709f2d7b655317409ab3c9a847e070fabaaa3
Waku <watsonmi188@gmail.com>
2020-12-09 23:10:24 +0000

    changed run dir

--- run.cpp
+++ run.cpp
@@ -38,7 +38,7 @@ int main (int argc, char** argv) {
 				no = "cd " + no; 
 				char *cstrx = &no[0];
 				char *cstrn = &z[0];
-				system(cstrx);
+				system("cd dist");
 				system(cstrx);
 				system(cstrn);
 				system("cd ../..");
